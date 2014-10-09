0d1af41da5b3a7d83044447aa69fc17867433da0
Alexander Mendez <bluespace@gmail.com>
2014-10-08 22:55:20 -0400

    working on cam

--- timerasp/camera.py
+++ timerasp/camera.py
@@ -6,17 +6,24 @@ import io
 import cv2
 import time
 import picamera
+import collections
 import numpy as np
 from PIL import Image
+from scipy import interpolate 
 
 
 RESOLUTION = (1280, 720)
 
 class Camera(object):
-    def __init__(self):
+    def __init__(self, navg=20):
         '''Start a camera Object'''
         self.cam = picamera.PiCamera()
         self.setup()
+        self.shutter = collections.deque(maxlen=navg)
+        self.red     = collections.deque(maxlen=navg)
+        self.blue    = collections.deque(maxlen=navg)
+        
+        
     
     def __enter__(self):
         return self
@@ -24,17 +31,68 @@ class Camera(object):
     def __exit__(self, type, value, traceback):
         self.cam.close()
     
-    def setup(self):
+    def getnext(self, array):
+        npts = len(array)
+        tmp = interpolate.UnivariateSpline(np.arange(npts), array)
+        return tmp(npts+1)
+    
+    def getshutter(self):
+        return getnext(self.shutter)
+    
+    def getawb(self):
+        return map(getnext, [self.red, self.blue])
+    
+    def setup(self, auto=False):
         self.cam.resolution = RESOLUTION
-        self.cam.exposure_mode = 'off'
-        self.cam.meter_mode = ''
         self.cam.image_effect = ''
-        self.cam.shutter_speed = 0
-        self.cam.iso = '100'
-        self.cam.awb_mode = 'off'
-        self.cam.awb_gains = (r,b)
+        self.cam.iso = 100
         self.cam.led = False
+        self.cam.meter_mode = 'average'
+        if auto:
+            self.cam.exposure_mode = 'auto'
+            self.cam.shutter_speed = 0
+            self.cam.awb_mode = 'auto'
+        else:
+            self.cam.awb_mode = 'off'
+            self.cam.awb_gains = self.getawb()
+            self.cam.exposure_mode = 'off'
+            self.cam.shutter_speed = self.getshutter()
+    
+    def update(self, img):
+        exp = camera.exposure_speed
+        r,b = self.cam.awb_gains
+        self.red.append(r)
+        self.blue.append(b)
+        self.shutter.append(exp)
         
+    
+    
+    def smoothcap(self):
+        ''' Generate a series of images that have a smooth
+        ramping of white balance, and exposure.  Procedure is 
+        to take a auto image pull out the measured awb and exposure
+        add those to a an array and use the average to then capture
+        a good image.  The size of the array will determine how 
+        smooth the resulting timelapse will be, and how quickly it
+        will respond to changes.'''
+        
+        while True:
+            # Capture test image
+            self.setup(auto=True)
+            time.sleep(1)
+            tmp = self.capture()
+            self.update(tmp)
+        
+            # capture real image
+            self.setup()
+            image = self.capture()
+            yield image
+    
+    
+    
+    
+    
+    
     def capture(self, annotate):
         self.cam.exif_tags['IFD0.Artist'] = ''
         self.cam.exif_tags['IFD0.Copyright'] = ''
