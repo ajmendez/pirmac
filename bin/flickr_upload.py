cbb132465d83474dd97c920c1fb28327e7971a0b
Alexander Mendez <bluespace@gmail.com>
2014-12-31 18:44:03 -0500

    fixing flickr

--- bin/flickr_upload.py
+++ bin/flickr_upload.py
@@ -1,22 +1,39 @@
 #!/usr/bin/env python
 
+import os
 import sys
+import json
 from timerasp import flickr
 
-def upload():
-    try:
-        filename = sys.argv[1]
-    except:
-        raise ValueError('Need to specify filename')
-    
-    
+def upload(filename):
     title = ''
     description = ''
-    if len(sys.argv) > 2:
-        description = ' '.join(sys.argv[2:])
+    tags = 'timerasp upload'
     
-    flickr.upload(filename, title, description, '')
+    # attempt to find a information file
+    infofile = filename.replace('_video.mp4', '_info.json')
+    if os.path.exists(infofile):
+        info = json.load(open(infofile, 'r'))
+        title = info['title']
+        description = info['description']
+        tags = info['tags']
+    
+    tmp = 'Title: {title}\n\nDescription: {description}\n\nTags: {tags}'
+    print tmp.format(**locals())
+    
+    if raw_input('upload? [y/n] ').lower()[0] != 'y':
+        print 'Not uploading!'
+        return
+    
+    flickr.upload(filename, title, description, tags)
 
 
 if __name__ == '__main__':
-    upload()
+    try:
+        filename = sys.argv[1]
+    except Exception as e:
+        raise ValueError('Pass in a filename')
+    if not os.path.exists(filename):
+        raise IOError('Missing file: {}'.format(filename))
+    
+    upload(filename)
--- timerasp/flickr.py
+++ timerasp/flickr.py
@@ -13,12 +13,20 @@ from poster.streaminghttp import register_openers
 class Progress(object):
     def __init__(self):
         self._seen = 0.0
+        self._pct = []
 
     def update(self, total, size, name):
         self._seen += size
-        pct = (self._seen / total) * 100.0
-        if (pct%10) == 0:
-            print '%s progress: %.2f' % (name, pct)
+        pct = int((self._seen / total) * 100)
+        if (pct not in self._pct) and ((pct%1) == 0) and (pct > 0):
+            self._pct.append(pct)
+            if (pct%10 == 0):
+                print '%s progress: %.2f%%' % (name, pct)
+            else:
+                sys.stdout.write('.')
+                sys.stdout.flush()
+                
+            
 
 class file_with_callback(file):
     def __init__(self, path, mode, callback, *args):
@@ -39,6 +47,7 @@ class file_with_callback(file):
 
 
 
+
 def upload(filename, title, description, tags, private=True):
     x = api.Flickr().flickr
     arguments = {'auth_token': x.token_cache.token, 
@@ -46,12 +55,13 @@ def upload(filename, title, description, tags, private=True):
                  'title':title,
                  'description':description,
                  'tags':tags,
+                 'async':'1',
                  'is_public':'0' if private else '1'}
     kwargs = flickrapi.make_utf8(arguments)
     kwargs['api_sig'] = x.sign(kwargs)
     url = "https://%s%s" % (x.flickr_host, x.flickr_upload_form)
 
-
+    
     register_openers()  
     progress = Progress()
     fh = file_with_callback(filename, 'rb',progress.update, os.path.basename(filename))
@@ -59,7 +69,26 @@ def upload(filename, title, description, tags, private=True):
     body2.update(kwargs)
     datagen, headers = multipart_encode(body2)
     request = urllib2.Request(url, datagen, headers)
-    return urllib2.urlopen(request).read()
+    
+    
+    print 'Uploading...'
+    
+    debug = False
+    if debug:
+        from pymendez import setup_stop
+        setup_stop()
+
+    for i in range(3):
+        response = urllib2.urlopen(request)
+        break
+    
+    print 'Done!'
+    output = response.read()
+    if debug:
+        print response
+        print output
+    
+    return output
 
 
 
