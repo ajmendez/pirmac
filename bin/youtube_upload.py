fe046de14542653f4b31f0143377459f0eafe0b3
Alexander Mendez <bluespace@gmail.com>
2015-01-12 07:41:11 -0500

    working on time

--- bin/youtube_upload.py
+++ bin/youtube_upload.py
@@ -1,7 +1,23 @@
 #!/usr/bin/env python
 '''youtube upload file'''
+import os
 import sys
+import json
 from timerasp import youtube
 
 if __name__ == '__main__':
-    youtube.upload(sys.argv[1], 'debug', 'debug video', 'debug,timerasp', True)
+    params = dict(
+        title='debug',
+        description='debug video',
+        tags='debug, timerasp',
+        private=True,
+    )
+    infofile = sys.argv[1].replace('.mp4', '.json')
+    if os.path.exists(infofile):
+        print "loading info from: {}".format(infofile)
+        with open(infofile,'r') as f:
+            tmp = json.load(f)
+        for key in ['title', 'description', 'tags']:
+            params[key] = tmp[key]
+    
+    youtube.upload(sys.argv[1], **params)
