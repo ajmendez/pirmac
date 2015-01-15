06a62b4cb152088da7ecd0880dc190fea85dccdd
Alexander Mendez <bluespace@gmail.com>
2015-01-15 14:38:39 -0500

    added delay for notify

--- bin/notify.py
+++ bin/notify.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 import sys
+import time
 from timerasp import gmail
 
 
 if __name__ == '__main__':
+    time.sleep(300)
     gmail.send_email('notify.py: IP:{}'.format(' '.join(sys.argv[1:])),
                      'Notify was run and is telling you something')
